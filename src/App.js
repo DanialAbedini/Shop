bae4075daebb359f7e89e1812d8331fa86c11162
danial <danial@gmail.com>
2021-11-15 18:08:43 +0330

    cart context added to project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Routes,Route,Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 //! Components
 import ProductsContextProvider from "./contexts/ProductsContextProvider";
 import ProductsDetails from "./Components/ProductsDetails";
 
-//! components 
+//! components
 import Products from "./Components/Products";
+import CartContextProvider from "./contexts/CartContextProvider";
 const App = () => {
-  return <div>
-    <ProductsContextProvider>
-      <Routes>
-        <Route path="/products" element={<Products/>}/>
-        <Route path="/products/:id" element={<ProductsDetails/>}/>
-        <Route path="*" element={<Navigate to={<Products/>}/>}/>
-      </Routes>
-    </ProductsContextProvider>
-  </div>;
+  return (
+    <div>
+      <ProductsContextProvider>
+        <CartContextProvider>
+          <Routes>
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductsDetails />} />
+            <Route path="*" element={<Navigate to={<Products />} />} />
+          </Routes>
+        </CartContextProvider>
+      </ProductsContextProvider>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/contexts/CartContextProvider.js
@@ -0,0 +1,83 @@
+import React, { useReducer } from "react";
+
+export const CartProvider = React.createContext();
+
+const initialState = {
+  selectedItems: [],
+  totalPrice: 0,
+  totalCount: 0,
+  checkout: false,
+};
+
+const cartRuduce = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
+        state.selectedItems.push({
+          ...action.payload,
+          quntity: 1,
+        });
+      }
+      return {
+        ...state,
+        checkout: false,
+        selectedItems: [...state.selectedItems],
+      };
+    case "REMOVE_ITEM":
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        checkout: false,
+        selectedItems: [...newSelectedItems],
+      };
+
+    case "INCREASE":
+      const indexI = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[indexI].quntity++;
+      return {
+        ...state,
+        checkout: false,
+      };
+
+    case "DECREASE":
+      const indexD = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[indexD].quntity--;
+      return {
+        ...state,
+        checkout: false,
+      };
+    case "CLEAR":
+      return {
+        selectedItems: [],
+        totalPrice: 0,
+        totalCount: 0,
+        checkout: false,
+      };
+    case "CHECKOUT":
+      return {
+        selectedItems: [],
+        totalPrice: 0,
+        totalCount: 0,
+        checkout: true,
+      };
+    default:
+      return state;
+  }
+};
+
+const CartContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartRuduce, initialState);
+  return (
+    <CartProvider.Provider value={{ state, dispatch }}>
+      {children}
+    </CartProvider.Provider>
+  );
+};
+
+export default CartContextProvider;
